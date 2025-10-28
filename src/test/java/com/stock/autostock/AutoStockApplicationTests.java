852ea8790116828cbcc52108b34ea5ee9b9d8c05
toychip <dlawnsgud427@naver.com>
2025-10-28 13:44:28 +0900

    Initial Commit

---
+++ src/test/java/com/stock/autostock/AutoStockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stock.autostock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoStockApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
